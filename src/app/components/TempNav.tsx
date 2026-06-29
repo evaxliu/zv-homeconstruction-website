1e64021b2c49acd805542b3ac36cb6f7933040a2
evaxliu <evaxinerliu@gmail.com>
2026-06-29 13:26:14 -0700

    remove unnecessary font css

--- src/app/components/TempNav.tsx
+++ src/app/components/TempNav.tsx
@@ -20,15 +20,15 @@ export default function TempNav() {
     <header className='bg-black sticky top-0 z-50'>
       <nav className='mx-auto max-w-7xl flex items-center justify-between py-6 px-6 bg-white'>
         <div className="flex text-slate-600">
-          <span className="font-serif text-xl tracking-tight">Z & V</span>
+          <span className="font-serif text-xl tracking-tight select-none">Z & V</span>
         </div>
         <div className='gap-6 hidden md:block'>
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className='text-slate-600 hover:text-slate-800 text-font-sans px-3 py-2 text-sm font-medium'>
+            <a key={item.name} href={item.href} className='text-slate-600 hover:text-slate-800 px-3 py-2 text-sm font-medium'>
               {item.name}
             </a>
           ))}
-          <a key={"Get a Quote"} href={"/"} className='text-slate-600 hover:text-white hover:bg-slate-800 border text-font-sans px-3 py-2 text-sm font-medium'>
+          <a key={"Get a Quote"} href={"/"} className='text-slate-600 hover:text-white hover:bg-slate-800 border px-3 py-2 text-sm font-medium'>
             Get a Quote
           </a>
         </div>
