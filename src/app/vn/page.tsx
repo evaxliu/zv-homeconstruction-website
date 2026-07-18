3d8ec9c0a52a6f545c0547f4421c7c8954fa2abc
evaxliu <evaxinerliu@gmail.com>
2026-07-18 16:09:40 -0700

    Initialize VN language support

---
+++ src/app/vn/page.tsx
@@ -0,0 +1,27 @@
+import AboutUs from "../components/sections/AboutUs";
+import Expertise from "../components/sections/Expertise";
+import Main from "../components/sections/Main";
+import Projects from "../components/sections/Projects";
+import Services from "../components/sections/Services";
+
+export default function Home() {
+  return (
+    <>
+      <div className="w-full">
+        <Main />
+      </div>
+      <div className="w-full">
+        <Services />
+      </div>
+      <div className="w-full">
+        <Expertise />
+      </div>
+      <div className="w-full">
+        <AboutUs />
+      </div>
+      <div className="w-full">
+        <Projects />
+      </div>
+    </>
+  );
+}
