ba476b09bfe0094a2fd59d53b7fc89d93f27635b
evaxliu <evaxinerliu@gmail.com>
2026-07-18 16:09:12 -0700

    Add VN language support

--- src/app/components/reusable/NavBar.tsx
+++ src/app/components/reusable/NavBar.tsx
@@ -1,27 +1,21 @@
 "use client"
 import Link from "next/link";
-import { useState } from "react";
 import { usePathname } from 'next/navigation'
 
 const language = [
-  { language: "EN", href: "/", current: true },
-  { language: "VN", href: "/", current: false }
+  { language: "EN", href: "/" },
+  { language: "VN", href: "/vn" }
 ]
 
 export default function NavBar() {
   const pathname = usePathname()
-  const [currentLang, setCurrentLang] = useState("EN")
-
-  const handleActiveLang = (language: string) => {
-    setCurrentLang(language);
-  };
   
   return (
     <header className="bg-slate-200 sticky top-0 z-50">
       <nav aria-label="Main navigation" className="mx-auto max-w-7xl flex items-center justify-between py-6 px-6">
         <div className="flex">
           <Link
-            href="/"
+            href={pathname === '/' ? "/" : "/vn"}
             className={`link ${pathname === '/' ? 'active' : ''} flex gap-3 text-2xl tracking-tight select-none text-[#4E8CA2]`}
           >
             <p className="font-semibold">Z <span className="text-[#202A36]">&amp;</span> V</p>
@@ -30,23 +24,37 @@ export default function NavBar() {
         </div>
         <div className="flex items-center gap-3 select-none">
           <Link 
-            key={currentLang == "EN" ? "Get a Quote" : "Tiếp xúc"}
+            key={pathname === '/' ? "Get a Quote" : "Tiếp xúc"}
             href={"/contact-us"} 
             className={"text-[#202A36] hover:bg-[#4E8CA2] border border-[#4E8CA2] px-3 py-2 text-sm font-semibold rounded-xl"}
           >
-            {currentLang == "EN" ? "Get a Quote" : "Tiếp xúc"}
+            {pathname === '/' ? "Get a Quote" : "Tiếp xúc"}
           </Link>
-          <div className="flex text-sm border border-[#4E8CA2] rounded-xl">
-            {language.map((item) => (
-              <Link 
-                key={item.language}
-                href={item.href} 
-                onClick={() => handleActiveLang(item.language)} 
-                className={(currentLang == item.language ? "text-[#202A36] bg-[#4E8CA2] " : "text-[#202A36] ") + "hover:bg-[#4E8CA2] font-semibold px-3 py-2 rounded-xl"}
-              >
-                {item.language}
-              </Link>
-            ))}
+          <div className="flex overflow-hidden rounded-xl border border-[#4E8CA2] text-sm">
+            {language.map((item) => {
+              const isVietnamese =
+                pathname === "/vn" || pathname.startsWith("/vn/");
+
+              const isActive =
+                item.language === "VN" ? isVietnamese : !isVietnamese;
+
+              return (
+                <Link
+                  key={item.language}
+                  href={item.href}
+                  className={`
+                    px-3 py-2 font-semibold
+                    hover:bg-[#4E8CA2]
+                    ${isActive
+                      ? "bg-[#4E8CA2] text-white"
+                      : "text-[#202A36]"
+                    }
+                  `}
+                >
+                  {item.language}
+                </Link>
+              );
+            })}
           </div>
         </div>
       </nav>
