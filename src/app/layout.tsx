d70c03e4dfe9ff3c24cfcf5f9b7df654ce7cf495
evaxliu <evaxinerliu@gmail.com>
2026-07-20 14:41:55 -0700

    Remove antialiased because its affecting text weight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className="h-full antialiased"
+      className="h-full"
     >
       <body className="min-h-full flex flex-col">
         <NavBar />
