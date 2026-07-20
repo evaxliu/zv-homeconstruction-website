a2d8118d2d8e6622cb9c51c5d993ca18db979743
evaxliu <evaxinerliu@gmail.com>
2026-07-20 14:03:49 -0700

    Changed font and text size to look more like design plan

--- src/app/components/sections/Main.tsx
+++ src/app/components/sections/Main.tsx
@@ -5,18 +5,19 @@ export default function Main() {
     <section className="w-full">
       <div className="mx-auto max-w-7xl px-6 py-10 min-h-[calc(100svh-80px)] flex flex-col justify-evenly">
         <div className="flex justify-end">
-          <h1 className="max-w-lg text-right text-2xl font-bold text-[#202A36]">
+          <h1 className="max-w-sm md:max-w-lg text-right text-2xl font-semibold text-[#202A36]">
             Experienced & Reliable Builders, proudly serving since 2021 with dedication and quality craftsmanship
           </h1>
         </div>
         <div className="flex flex-col md:flex-row md:justify-between md:items-end">
-          <p className="max-w-md text-5xl md:text-7xl font-bold text-[#202A36]">
+          <p className="max-w-sm md:max-w-xl text-7xl md:text-8xl font-bold text-[#202A36]">
             Your satisfaction is our goal
           </p>
           <div 
-            className="hidden md:flex items-center gap-2 text-[#202A36] text-sm font-semibold rounded-xl animate-bounce select-none"
+            className="hidden lg:flex items-center gap-2 text-[#202A36] text-lg font-semibold rounded-xl animate-bounce select-none"
           >
-            Scroll for More <ArrowDown />
+            <span>Scroll for More</span>
+            <ArrowDown size={28} strokeWidth={1.75} />
           </div>
         </div>
       </div>
