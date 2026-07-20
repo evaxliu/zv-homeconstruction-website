78c44d7ddc6b66db2300fbc3e30db7a173ebc554
evaxliu <evaxinerliu@gmail.com>
2026-07-20 13:08:27 -0700

    Remove vn version.

--- src/app/components/reusable/NavBar.tsx
+++ src/app/components/reusable/NavBar.tsx
@@ -1,15 +1,7 @@
 "use client"
 import Link from "next/link";
-import { usePathname } from 'next/navigation'
 
-const language = [
-  { language: "EN", href: "/" },
-  { language: "VN", href: "/vn" }
-]
-
-export default function NavBar() {
-  const pathname = usePathname()
-  
+export default function NavBar() {  
   const scrollToSection = () => {
     document
       .getElementById("contact")
@@ -17,12 +9,12 @@ export default function NavBar() {
   };
 
   return (
-    <header className="bg-slate-200 sticky top-0 z-50">
+    <header className="bg-[#E5ECF2] sticky top-0 z-50">
       <nav aria-label="Main navigation" className="mx-auto max-w-7xl flex items-center justify-between py-6 px-6">
         <div className="flex">
           <Link
-            href={pathname === '/' ? "/" : "/vn"}
-            className={`link ${pathname === '/' ? 'active' : ''} flex gap-3 text-2xl tracking-tight select-none text-[#4E8CA2]`}
+            href={"/"}
+            className="flex gap-3 text-2xl tracking-tight select-none text-[#4E8CA2]"
           >
             <p className="font-semibold">Z <span className="text-[#202A36]">&amp;</span> V</p>
             <p className="text-[#202A36] hidden lg:flex text-2xl tracking-tight select-none">Home Construction LLC</p>
@@ -30,38 +22,12 @@ export default function NavBar() {
         </div>
         <div className="flex items-center gap-3 select-none">
           <button 
-            key={pathname === '/' ? "Get a Quote" : "Tiếp xúc"}
+            key={"Get a Quote"}
             onClick={scrollToSection}
             className={"text-[#202A36] hover:bg-[#4E8CA2] hover:text-white border border-[#4E8CA2] px-3 py-2 text-sm font-semibold rounded-xl"}
           >
-            {pathname === '/' ? "Get a Quote" : "Tiếp xúc"}
+            Get a Quote
           </button>
-          <div className="flex overflow-hidden rounded-xl border border-[#4E8CA2] text-sm">
-            {language.map((item) => {
-              const isVietnamese =
-                pathname === "/vn" || pathname.startsWith("/vn/");
-
-              const isActive =
-                item.language === "VN" ? isVietnamese : !isVietnamese;
-
-              return (
-                <Link
-                  key={item.language}
-                  href={item.href}
-                  className={`
-                    px-3 py-2 font-semibold
-                    hover:bg-[#4E8CA2]
-                    ${isActive
-                      ? "bg-[#4E8CA2] text-white"
-                      : "text-[#202A36]"
-                    }
-                  `}
-                >
-                  {item.language}
-                </Link>
-              );
-            })}
-          </div>
         </div>
       </nav>
     </header>
